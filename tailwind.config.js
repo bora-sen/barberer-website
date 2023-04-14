046b4182377549e851c02db1b252baeae53d8d4a
bora-sen <bahattinborasen@gmail.com>
2023-04-14 16:13:14 +0300

    +update Theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,15 @@ export default {
   theme: {
     extend: {
       colors: {
-        "primary-black": "#000",
-        "primary-white": "#FFF",
-        "primary-red": "#F00",
-        "primary-blue": "#00F",
+        "primary-black": "#3b2c21",
+        "primary-white": "#fefefd",
+        "primary-red": "#ad2f3c",
+        "primary-blue": "#5DD9CC",
         "primary-green": "#0F0",
+        "primary-green-darker":"#8c714a"
+      },
+      backgroundImage: {
+        "create-booking": "url(Assets/Images/booking-bg-placeholder.webp)",
       },
     },
   },
