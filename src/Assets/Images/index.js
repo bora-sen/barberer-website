6294661d2fba61b43f58441f8f5586597d22c71c
bora-sen <bahattinborasen@gmail.com>
2023-04-14 16:12:32 +0300

    +added Assets

---
+++ src/Assets/Images/index.js
@@ -0,0 +1,10 @@
+export { default as Service1 } from "./services-1-image.jpg"
+export { default as Service2 } from "./services-2-image.jpg"
+export { default as Service3 } from "./services-3-image.jpg"
+
+export { default as FollowSocials } from "./follow-social-bg.jpg"
+
+export { default as BookingBg } from "./booking-bg-placeholder.webp"
+export { default as Booking } from "./booking-placeholder.webp"
+
+export { default as HeroBg } from "./hero-bg.jpg"
