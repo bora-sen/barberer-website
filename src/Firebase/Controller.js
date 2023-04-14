c42930e1dc3124c904234ffd83ad5e257db81255
bora-sen <bahattinborasen@gmail.com>
2023-04-14 16:12:44 +0300

    +updated Firebase Controller

--- src/Firebase/Controller.js
+++ src/Firebase/Controller.js
@@ -1,5 +1,5 @@
 import { uuidv4 } from "@firebase/util"
-import { collection, doc, getDocs, query, setDoc } from "firebase/firestore"
+import { collection, doc, getDocs, query, setDoc, where } from "firebase/firestore"
 import { db } from "./index"
 
 export async function addNewBooking(bookingObj) {
@@ -25,6 +25,36 @@ export async function getAllBookings() {
       BookingsArr.push(snap.data())
     })
     return BookingsArr
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export async function getBookingsByDate(bookingDate) {
+  try {
+    const books = []
+    const snapShot = await getDocs(query(collection(db, "bookings"), where("bookingDate", "==", bookingDate)))
+    snapShot.forEach((snap) => {
+      books.push(snap.data())
+    })
+    return books
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export async function getAvailableBookingHoursByDate(bookingDate) {
+  try {
+    const allHours = ["11:00", "11:30", "12:00", "12:30", "13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00", "17:30", "18:00"]
+    const bookedHours = []
+
+    const bookings = await getBookingsByDate(bookingDate)
+    bookings.forEach((booking) => {
+      bookedHours.push(booking.bookingHour)
+    })
+
+    const bookableHours = allHours.filter((hour) => !bookedHours.includes(hour))
+    return bookableHours
   } catch (error) {
     console.log(error)
   }
